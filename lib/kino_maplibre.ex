8a98863c225967a3e2460019c01319fc2b750c58
Cristine Guadelupe <cristineguadelupe@me.com>
2022-05-04 19:01:05 -0300

    Initial commit

---
+++ lib/kino_maplibre.ex
@@ -0,0 +1,18 @@
+defmodule KinoMaplibre do
+  @moduledoc """
+  Documentation for `KinoMaplibre`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KinoMaplibre.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kino_maplibre_test.exs
@@ -0,0 +1,8 @@
+defmodule KinoMaplibreTest do
+  use ExUnit.Case
+  doctest KinoMaplibre
+
+  test "greets the world" do
+    assert KinoMaplibre.hello() == :world
+  end
+end
