b45069c89c978b3f2f77df01b585531f7903445a
Cristine Guadelupe <cristineguadelupe@me.com>
2022-05-07 00:01:18 -0300

    Render impl for Kino.Maplibre

--- lib/kino_maplibre.ex
+++ lib/kino_maplibre.ex
@@ -3,3 +3,9 @@ defimpl Kino.Render, for: Maplibre do
     ml |> Kino.Maplibre.static() |> Kino.Render.to_livebook()
   end
 end
+
+defimpl Kino.Render, for: Kino.Maplibre do
+  def to_livebook(ml) do
+    ml |> Kino.Maplibre.static() |> Kino.Render.to_livebook()
+  end
+end
