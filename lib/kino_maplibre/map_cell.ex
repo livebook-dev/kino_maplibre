d460b4fefa1488bd7a60080a316f579c1b9ea0fb
Cristine Guadelupe <cristineguadelupe@me.com>
2022-05-20 22:28:37 -0300

    Map cell - initial implementation

--- lib/kino_maplibre/map_cell.ex
+++ lib/kino_maplibre/map_cell.ex
@@ -5,39 +5,139 @@ defmodule KinoMapLibre.MapCell do
   use Kino.JS.Live
   use Kino.SmartCell, name: "Map"
 
+  @as_int ["zoom"]
+  @as_atom []
+
   @impl true
-  def init(_attrs, ctx) do
+  def init(attrs, ctx) do
+    _layer = if attrs["layers"], do: List.first(attrs["layers"]), else: nil
+
+    fields = %{
+      "style" => attrs["style"],
+      "center" => attrs["center"],
+      "zoom" => attrs["zoom"]
+    }
+
+    ctx = assign(ctx, fields: fields, ml_alias: nil, missing_dep: missing_dep())
     {:ok, ctx, reevaluate_on_change: true}
   end
 
   @impl true
-  def scan_binding(_pid, _binding, _env) do
-    :ok
+  def scan_binding(pid, _binding, env) do
+    ml_alias = ml_alias(env)
+    send(pid, {:scan_binding_result, ml_alias})
   end
 
   @impl true
   def handle_connect(ctx) do
-    payload = %{}
+    payload = %{
+      fields: ctx.assigns.fields,
+      missing_dep: ctx.assigns.missing_dep
+    }
+
     {:ok, payload, ctx}
   end
 
   @impl true
-  def handle_info({:scan_binding_result, _data_options, _ml_alias}, ctx) do
+  def handle_info({:scan_binding_result, ml_alias}, ctx) do
+    ctx = assign(ctx, ml_alias: ml_alias)
     {:noreply, ctx}
   end
 
   @impl true
-  def handle_event("update_field", %{"field" => _field, "value" => _value}, ctx) do
+  def handle_event("update_field", %{"field" => field, "value" => value}, ctx) do
+    parsed_value = parse_value(field, value)
+    ctx = update(ctx, :fields, &Map.put(&1, field, parsed_value))
+    broadcast_event(ctx, "update", %{"fields" => %{field => parsed_value}})
     {:noreply, ctx}
   end
 
-  @impl true
-  def to_attrs(_ctx) do
-    %{}
+  defp parse_value(_field, ""), do: nil
+  defp parse_value(field, value) when field in @as_int, do: String.to_integer(value)
+  defp parse_value(_field, value), do: value
+
+  defp convert_field(field, nil), do: {String.to_atom(field), nil}
+
+  defp convert_field("center", center) do
+    [lng, lat] = center |> String.replace(" ", "") |> String.split(",")
+    {lng, _} = Float.parse(lng)
+    {lat, _} = Float.parse(lat)
+    {:center, {lng, lat}}
+  end
+
+  defp convert_field(field, value) when field in @as_atom do
+    {String.to_atom(field), String.to_atom(value)}
+  end
+
+  defp convert_field(field, value), do: {String.to_atom(field), value}
+
+  defp ml_alias(%Macro.Env{aliases: aliases}) do
+    case List.keyfind(aliases, MapLibre, 1) do
+      {ml_alias, _} -> ml_alias
+      nil -> MapLibre
+    end
   end
 
   @impl true
-  def to_source(_attrs) do
-    "map cell"
+  def to_attrs(ctx) do
+    ctx.assigns.fields
+    |> add_layer()
+    |> Map.put("ml_alias", ctx.assigns.ml_alias)
+  end
+
+  @impl true
+  def to_source(attrs) do
+    attrs
+    |> extract_layer()
+    |> to_quoted()
+    |> Kino.SmartCell.quoted_to_string()
+  end
+
+  defp to_quoted(attrs) do
+    attrs = Map.new(attrs, fn {k, v} -> convert_field(k, v) end)
+
+    [root | _nodes] = [
+      %{
+        field: nil,
+        name: :new,
+        module: attrs.ml_alias,
+        args: build_arg_root(style: attrs.style, center: attrs.center, zoom: attrs.zoom)
+      }
+    ]
+
+    root = build_root(root)
+    root
+  end
+
+  defp build_root(root) do
+    quote do
+      unquote(root.module).unquote(root.name)(unquote_splicing(root.args))
+    end
+  end
+
+  defp build_arg_root(opts) do
+    opts
+    |> Enum.filter(&elem(&1, 1))
+    |> case do
+      [] -> []
+      opts -> [opts]
+    end
+  end
+
+  defp missing_dep() do
+    unless Code.ensure_loaded?(MapLibre) do
+      ~s/{:maplibre, "~> 0.1.0"}/
+    end
+  end
+
+  defp add_layer(attrs) do
+    {root, layer} = Map.split(attrs, ["style", "center", "zoom"])
+    Map.put(root, "layers", [layer])
+  end
+
+  defp extract_layer(%{"layers" => [layer]} = attrs) do
+    attrs
+    |> Map.delete("layers")
+    |> Map.merge(layer)
   end
 end
