bd250088d5af205202b0c2931709d873ebf25b58
Cristine Guadelupe <cristineguadelupe@me.com>
2022-06-22 19:09:33 -0300

    Updates deps

--- mix.exs
+++ mix.exs
@@ -29,16 +29,15 @@ defmodule KinoMapLibre.MixProject do
     [
       {:kino, "~> 0.6.1"},
       {:table, "~> 0.1.0"},
-      {:maplibre, github: "cristineguadelupe/maplibre"},
-      {:ex_doc, "~> 0.28", only: :dev, runtime: false},
-      {:geo, "~> 3.4", only: :test}
+      {:maplibre, github: "livebook-dev/maplibre"},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 
   defp docs do
     [
       main: "components",
-      source_url: "https://github.com/cristineguadelupe/kino_maplibre",
+      source_url: "https://github.com/livebook-dev/kino_maplibre",
       source_ref: "v#{@version}",
       extras: ["guides/components.livemd"],
       groups_for_modules: [
