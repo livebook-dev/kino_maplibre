abb16203a39c34d18cd777ac9eb0acb11d37f3cb
José Valim <jose.valim@gmail.com>
2022-10-10 12:03:01 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule KinoMapLibre.MixProject do
 
   defp deps do
     [
-      {:kino, "~> 0.6"},
+      {:kino, "~> 0.6.1 or ~> 0.7.0"},
       {:table, "~> 0.1.0"},
       {:maplibre, "~> 0.1.2"},
       {:ex_doc, "~> 0.28", only: :dev, runtime: false}
